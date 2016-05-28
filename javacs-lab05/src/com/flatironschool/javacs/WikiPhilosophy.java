f02abb9d6bd3e84f583c79dbd253a50063c1295b
michellexchen <michellechen617@gmail.com>
2016-05-28 16:13:46 -0500

    completed lab

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -29,23 +29,64 @@ public class WikiPhilosophy {
 	 */
 	public static void main(String[] args) throws IOException {
 		
-        // some example code to get you started
-
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
+		String start = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		String goal = "https://en.wikipedia.org/wiki/Philosophy";
+		boolean found = false;
+		boolean nextLink = false;
+		List visited = new ArrayList<String>();
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+		while (!found) {
+			Elements paragraphs = wf.fetchWikipedia(start);
+			int parens = 0;
+			for (Element paragraph: paragraphs) {
+				Iterable<Node> iter = new WikiNodeIterable(paragraph);
+				for (Node node: iter) {
+					if (node instanceof TextNode) { //parenthesis
+						for (int x = 0; x<node.toString().length(); x++) {
+							if (Character.toString(node.toString().charAt(x)).equals("(")) {
+								parens++;
+							} else if (Character.toString(node.toString().charAt(x)).equals(")")){
+								parens--;
+							}
+						}
+					} else {
+						Element url = (Element) node;
+						if (url.tagName().equals("a")) { //found link
+							String next = url.attr("href");
+							if (Character.isUpperCase(url.text().charAt(0)) || parens !=0){ //capital letter, parens
+								continue;
+							}
+							if (!next.contains("/wiki") || start.substring(start.indexOf("/wiki")).equals(next)) { //external link, current page
+								continue;
+							}
+							for (Element parent: url.parents()) { //italics
+								if (parent.tagName().equals("em") || parent.tagName().equals("i")) {
+									continue;
+								}
+							}
+							start = "https://en.wikipedia.org"+next;
+							if (visited.contains(start)) {
+								continue;
+							}
+							System.out.println("START IS: " + start);
+							visited.add(start);
+							if (start.equals(goal)) {
+								System.out.println("congrats! Philosophy page found.");
+								visited.add(start);
+								System.out.println(visited.toString());
+								return;
+							}
+							nextLink = true;
+							break;
+						}												
+					}
+				}
+				if (nextLink) {
+					break;
+				}
 			}
-        }
-
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+		}
+		System.out.println("search failed.");
+		System.out.println(visited.toString());
 	}
 }
